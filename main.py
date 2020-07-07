700d1da6cf7406b2d3eda5b10f1468e50ec73f11
shwetagawai2120 <67950411+shwetagawai2120@users.noreply.github.com>
2020-07-07 19:35:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ session = InstaPy(
 
 session.login()
 
-#session.set_do_comment(True, percentage=10)
+'''session.set_do_comment(True, percentage=10)
 session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
 
-'''session.set_do_comment(True, percentage=25)
+session.set_do_comment(True, percentage=25)
 session.set_comments(['Nice Picture!!','Good Shot','Damnn its nice :)','Good One!','Keep it up!'])'''
 
 #Follow people based on the stated hashtags below
-session.follow_by_tags(['diettips'], amount=0)
+session.follow_by_tags(['diettips','stayhealthy','eathealthy'], amount=1)
 
 #Like Post using hashtags given below
-#session.like_by_tags(['diettips'], amount=0)
+session.like_by_tags(['diettips','stayhealthy','eathealthy'], amount=1)
 
-#Comments on post based on
-#session.set_do_comment(True, percentage=25)
-#session.set_comments(['Nice Picture!!','Good Shot','Damnn its nice :)','Good One!','Keep it up!'], media='Photo')
 
-session.set_delimit_commenting(enabled=True, comments_mandatory_words=['stayfit', 'diettips'])
+'''session.set_do_comment(True, percentage=25)
+session.set_comments(['Nice Picture!!','Good Shot','Damnn its nice :)','Good One!','Keep it up!'], media='Photo')
+
+session.set_delimit_commenting(enabled=True, comments_mandatory_words=['stayfit', 'diettips'])'''
 
 
 #Unfollow users who don't follow back
